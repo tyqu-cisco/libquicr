2cb5a0efc0f3d9f74ac0975bc2448fa55596272a
Richard Barnes <rlb@ipv.sx>
2023-09-15 14:40:55 -0400

    Streamline test files
    * Remove the unneded common_utils struct
    * Inline the test case into main.cpp
    * Remove unneeded participant vector and std::string conversions

--- mlstest/main.cpp
+++ mlstest/main.cpp
@@ -1,2 +1,53 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
+
+#include "logger.h"
+#include "namespace_config.h"
+#include "quicr_client_helper.h"
+
+#include <thread>
+
+TEST_CASE("Two person test using quicr and mls")
+{
+  auto logger = Logger{};
+  const auto nspace_config = NamespaceConfig::create_default();
+  auto creator = QuicrClientHelper{ "FFFOOO", logger, true };
+  auto joiner = QuicrClientHelper{ "FFFOO1", logger, false };
+
+  // subscribe for all
+  const auto namespaces = std::vector<quicr::Namespace>{
+    nspace_config.key_package,
+    nspace_config.welcome,
+    nspace_config.commit,
+  };
+  for (const auto& ns : namespaces) {
+    if (ns != nspace_config.welcome) {
+      creator.subscribe(ns, logger);
+      logger.log(qtransport::LogLevel::info,
+                       "Creator, Subscribing  to namespace " + ns.to_hex());
+    }
+    joiner.subscribe(ns, logger);
+    logger.log(qtransport::LogLevel::info,
+                     "Subscribing to namespace " + ns.to_hex());
+  }
+
+  // participant publish keypackage
+  if (!joiner.isUserCreator()) {
+    auto name = nspace_config.key_package.name();
+    logger.log(qtransport::LogLevel::info,
+                     "Publishing to " + name.to_hex());
+    joiner.publishJoin(name);
+  }
+
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+  logger.log(qtransport::LogLevel::info,
+                   "Sleeping for 10 seconds for mls handshake to complete");
+
+  CHECK_EQ(creator.getSession().get_state(),
+           joiner.getSession().get_state());
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  logger.log(qtransport::LogLevel::info,
+                   "Sleeping for 5 seconds before unsubscribing");
+}
+
